cdfe952c9d0180ec9fa13ba01c2aeb2e64ddf66e
Erebuss0 <109722528+Erebuss0@users.noreply.github.com>
2026-02-25 20:27:12 +0300

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -215,7 +215,7 @@ class GameManager:
 
 game = GameManager()
 app = FastAPI()
-app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=False, allow_methods=["*"], allow_headers=["*"])
 
 @app.post("/api/register")
 def register_user(user: UserAuth):
